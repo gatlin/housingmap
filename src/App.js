7eb061279f11fdac09848393d99bb405c2d5eec9
Gatlin Johnson <gatlin@niltag.net>
2018-09-06 13:28:56 -0500

    frist

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { HousingMap } from './HousingMap';
+
+const App = props => (
+    <HousingMap/>
+);
+
+export default App;
---
+++ src/HousingMap.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Map, TileLayer, Marker, Popup, Polygon } from 'react-leaflet';
+import './HousingMap.css';
+import coords from './coords';
+
+
+//    [30.252878, -97.767825],
+class HousingMap extends React.Component {
+    state = {
+        lat:  30.2711286,
+        lng: -97.7436995,
+        zoom: 11,
+        height: 0
+    };
+
+    updateDimensions = () => {
+        const height = window.innerHeight;
+        this.setState({ height });
+    };
+
+    componentWillMount() {
+        this.updateDimensions();
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.updateDimensions);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateDimensions);
+    }
+
+    render() {
+        const position = [this.state.lat, this.state.lng];
+        return (
+            <div id='map-wrapper' style={{height: this.state.height}}>
+              <Map
+                center={position}
+                zoom={this.state.zoom}
+                >
+                <TileLayer
+                  attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+                {
+                      coords.map(positions => (
+                          <Polygon
+                            color='black'
+                            positions={positions}/>
+                      ))
+                }
+              </Map>
+            </div>
+        );
+    }
+};
+
+export { HousingMap };
