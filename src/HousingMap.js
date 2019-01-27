33babfbfac04152b395a1344e6f7b0b05cfdeff5
Gatlin Johnson <gatlin@niltag.net>
2019-01-27 10:36:07 -0600

    layer controls working

--- src/HousingMap.js
+++ src/HousingMap.js
@@ -9,12 +9,13 @@ import {
     FeatureGroup,
     GeoJSON
 } from 'react-leaflet';
+
 import './HousingMap.css';
 import coords from './coords';
 import HeatmapLayer from 'react-leaflet-heatmap-layer';
 import * as data from './austindistricts.json';
 import * as alcohol from './no_public_consumption.json';
-
+const { BaseLayer, Overlay } = LayersControl;
 class HousingMap extends React.Component {
 
     state = {
@@ -59,16 +60,15 @@ class HousingMap extends React.Component {
 
     districtStyle = feature => {
         const colors = [
-            '#8b9ab7',
-            '#c88799',
-            '#3aced2',
-            '#848047',
-            '#0a7b3c',
-            '#aa523d',
-            '#287cd7',
-            '#31474e',
-            '#9ec18f',
-            '#7d13bf'
+            "#e41a1c",
+            "#377eb8",
+            "#4daf4a",
+            "#984ea3",
+            "#ff7f00",
+            "#ffff33",
+            "#a65628",
+            "#f781bf",
+            "#999999"
         ];
         console.log('[style]', feature.properties.council_district);
         return {
@@ -89,17 +89,20 @@ class HousingMap extends React.Component {
                     attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                  { this.renderDots(coords) }
-                  <GeoJSON
-                    opacity={0.35}
-                    data={data} color='purple'
-                    style={this.districtStyle}
-                    onEachFeature={this.onEachDistrict}
-                    />
-                  <GeoJSON
-                    opacity={0.7}
-                    data={alcohol} color='red'
-                    />
+                  <Overlay checked name="Austin Districts">
+                    <GeoJSON
+                      opacity={0.45}
+                      data={data} color='purple'
+                      style={this.districtStyle}
+                      onEachFeature={this.onEachDistrict}
+                      />
+                  </Overlay>
+                  <Overlay name='No Public Drinking'>
+                    <GeoJSON
+                      opacity={0.7}
+                      data={alcohol} color='red'
+                      />
+                  </Overlay>
                 </LayersControl>
               </Map>
             </div>
